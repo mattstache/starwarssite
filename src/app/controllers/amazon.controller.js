bcc932381035610bfe65ca3abe76e827a9c5eb46
Matt <mattalarie@gmail.com>
2018-01-05 23:12:25 -0800

    Start adding Amazon functionality including search and cart building

--- src/app/controllers/amazon.controller.js
+++ src/app/controllers/amazon.controller.js
@@ -1,23 +1,128 @@
 'use strict';
 
-const Piranhax = require("piranhax")
-const client = new Piranhax(process.env.AWS_ACCESS_KEY_ID, process.env.AMAZON_SECRET_KEY, process.env.AMAZON_ASSOCIATE_TAG)
+const _ = require("lodash");
+const Piranhax = require("piranhax");
+const client = new Piranhax(process.env.AWS_ACCESS_KEY_ID, process.env.AMAZON_SECRET_KEY, process.env.AMAZON_ASSOCIATE_TAG);
+
+var searchResult = {
+  Item: {
+    ASIN: '',
+    DetailPageURL: '',
+    ItemLinks: [
+      {
+        ItemLink:
+        {
+          Description: '',
+          URL: ''
+        }
+      }
+    ],
+    ItemAttributes: {
+
+    }
+  },
+  MoreSearchResultsURL: '', //  The URL that displays the complete search results. Use these URLs to link back to the item on Amazon. Each URL is tagged with your Associate tag and contains tracking information to increase your hourly request limit as the sales you generate increase.
+  TotalPages: '',
+  TotalResults: '',
+  SearchResultsMap: [
+    {
+      SearchIndex:{
+        IndexName: '',
+        Results: '',
+        Pages: '',
+        RelevanceRank: '',
+        ASIN: ['']
+      }
+    }
+  ]
+}
+
+
+var product = {
+  name: '',
+  description: '',
+  price: '',
+  amazonUrl:'',
+  ASIN: '',
+  inStock: '',
+  featured: '',
+  createdDate: '',
+  productType: '',
+  tagIDs: [],
+  images: {
+    small:{
+      url: '',
+      type: ''
+    },
+    medium: {
+      url: '',
+      type: ''
+    }
+  }
+}
 
 
 function searchProducts(searchIndex, keywords){
+  //https://docs.aws.amazon.com/AWSECommerceService/latest/DG/ItemSearch.html
   var products = client.ItemSearch(searchIndex, {
-    Keywords: keywords
+    Keywords: keywords,
+    ResponseGroup: ['OfferListings', 'Images', 'PromotionSummary']
   }).then(results => {
-      // results is a response object, see below for further information.
-      console.log(results.data())
+      console.log('==SEARCH RESPONSE==')
+      console.log(results.get("Item[0]"))
+      let Item = results.get("Item")
+      let OfferListingIds = getOfferListingIdsFromItem(Item);
+   
+      // map OfferListingId 
+      let AddToCartItems = getCartItems(OfferListingIds);
+   
+      // create request 
+      return createAmazonCart(AddToCartItems)
+  }).then(result => {
+    // get CartId 
+    let CartId = result.get("CartId")
+ 
+    // get HMAC and one of CartItemId 
+    let HMAC = result.get("HMAC")
+    let CartItemId = result.get("CartItems.CartItem[0].CartItemId")
 
-      // get first item ASIN
-      console.log(results.get("Item[0].ASIN"))
-  }).catch(err => {
+    console.log(result)
+  })
+  .catch(err => {
       console.log("Why error?", err)
   })
 
   return products;
+}
+
+function addItemsToAmazonCart(items){
+  let cartExists = false;
+
+  return cartExists ? createAmazonCart(items) : addItemsToExistingAmazonCart(items);
+}
+
+function addItemsToExistingAmazonCart(items){
+  // make sure you do getCartItems first
+  // get cartId from cookie and HMAC
+  //return client.CartAdd(items, CartId, HMAC)
+}
+
+function getCartItems(offerListingIds){
+  return _.map(offerListingIds, id => {
+      let item = client.CreateCartItem("OfferListingId", id, 1)
+      return item
+  })
+}
+
+function getOfferListingIdsFromItem(item){
+  return _.map(item, i =>
+  _.get(i, "Offers.Offer.OfferListing.OfferListingId"))
+  .filter(i => i !== undefined)
+}
+
+function createAmazonCart(items){
+  //set cartId and HMAC from this return in cookie
+  return client.CartCreate(items)
 }
 
 exports.search = function(req, res) {
